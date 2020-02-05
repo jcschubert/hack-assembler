21905251126ee4f0fb3c90a38d7fc6dee4d9e724
Jan-Christopher Schubert <jan@jcschubert.com>
2020-02-05 23:27:14 +0100

    Handle simple @instructions + test

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -1,9 +1,27 @@
 package parser
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Instruction struct {
-	raw string
+	iType string
+	value int
 }
 
 func Parse(lines []string) (instructions []Instruction) {
-	return []Instruction{}
+	result := []Instruction{}
+	for _, line := range lines {
+		line := strings.Trim(line, "\n")
+		if strings.HasPrefix(line, "@") {
+			value, err := strconv.Atoi(line[1:])
+			if err != nil {
+				panic(err)
+			}
+			instruction := Instruction{"@", value}
+			result = append(result, instruction)
+		}
+	}
+	return result
 }
--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -15,4 +15,15 @@ func TestParser(t *testing.T) {
 			t.Fatalf("Parse(%+v) should return %+v, but returned %+v", input, want, got)
 		}
 	})
+	t.Run("A single @instruction returns an @instruction", func(t *testing.T) {
+		input := []string{"@1234"}
+		want := []Instruction{
+			{"@", 1234},
+		}
+		got := Parse(input)
+
+		if !reflect.DeepEqual(want, got) {
+			t.Fatalf("Parse(%+v) should return %+v, but returned %+v", input, want, got)
+		}
+	})
 }
